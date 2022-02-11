fcff3c48871b7813be29e39a7c0fd68244a9dd39
Onassis <onassis.sowah@gmail.com>
2022-02-11 16:45:26 +0100

    created the note app with some styles

---
+++ index.js
@@ -0,0 +1,20 @@
+const noteListDiv = document.querySelector('.note-list')
+let noteID = 1
+const Note = (id, title, content) => {
+    this.id = id;
+    this.title =title;
+    
+}
+
+
+// eventlisteners
+
+function eventListeners (){
+document.getElementById('add-note-btn').addEventListener
+('click', addNewNote)
+}
+
+eventListeners()
+
+
+
